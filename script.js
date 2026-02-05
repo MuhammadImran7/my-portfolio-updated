b6cda44c841f5745fa78f9f57de3b64d7ec374f2
imran138 <muhammmad.imran@ki5.co.uk>
2026-02-05 16:30:12 +0500

    Initial Portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scrolling
+const links = document.querySelectorAll('a[href^="#"]');
+links.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
+});
